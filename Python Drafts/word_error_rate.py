81f85c16e187b74e3d436115a363621cfc676f96
joshuaning <joshning@umich.edu>
2024-04-01 12:27:15 -0400

    minor edits

--- Python Drafts/word_error_rate.py
+++ Python Drafts/word_error_rate.py
@@ -6,11 +6,11 @@ from scipy.io import wavfile
 model = whisper.load_model("base.en")
 
 # Load an audio file
-file_path = "Sounds/Sentences/no_noise_sentence_1.wav"
+file_path = "Sounds/Sentences/Twigs_sentence_1.wav"
 audio = whisper.load_audio(file_path)
 audio = whisper.pad_or_trim(audio)
 
-# use whuspere to transcribe the audio
+# use Whisper to transcribe the audio
 result = whisper.transcribe(model, audio)
 
 print(result)
